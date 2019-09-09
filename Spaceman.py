9948b0dc3e6806bafbd60da55bcb790457620dfd
Student Laptop_7/19_1 <studentlaptop_719_1@Students-MacBook-Pro.local>
2019-09-08 20:22:38 -0700

    Finish Spaceman

--- Spaceman.py
+++ Spaceman.py
@@ -1,4 +1,4 @@
-# Helped by Francis
+# Helped by Audi
 import random
 
 def load_word():
@@ -73,17 +73,46 @@ def spaceman(secret_word):
     Args:
       secret_word (string): the secret word to guess.
     '''
+    remaining_guesses = len(secret_word)
+    letters_guessed = []
+
+    running = True
+    while running:
+        guessing = True
+        while guessing:
+            guess = input("Guess A Letter: ")
+            if guess.isalpha() and len(guess) == 1:
+                letters_guessed.append(guess)
+                guessing = False
+            else:
+                print("Incorrect Input")
+
+        guessed_word = get_guessed_word(secret_word, letters_guessed)
+        if is_guess_in_word(guess, secret_word):
+             print("Correct Guess")
+        else:
+            print("Incorrect Guess")
+            remaining_guesses -= 1
+            print("Guesses Left: " + str(remaining_guesses))
+
+        print(guessed_word)
+
+        if is_word_guessed(secret_word, letters_guessed):
+            print("Game Won")
+            running = False
+        elif remaining_guesses == 0:
+            print("Game Lost")
+            running = False
 
 
-    #TODO: show the player information about the game according to the project spec
 
-    #TODO: Ask the player to guess one letter per round and check that it is only one letter
+#TODO: Show game information based on project spec
+#TODO: Ask player to guess 1 letter/round and check if only 1 letter
+#TODO: Check if guessed letter is in/not in secret_word
+#TODO: Show guessed letter (so far)
+#TODO: Check if game has been Won/Lost
 
-    #TODO: Check if the guessed letter is in the secret or not and give the player feedback
 
-    #TODO: show the guessed word so far
-
-    #TODO: check if the game has been won or lost
 
 
 
@@ -91,8 +120,13 @@ def spaceman(secret_word):
 
 
 #These function calls that will start the game
-letters_guessed = ["a"]
-secret_word = "apple"
-get_guessed_word(secret_word, letters_guessed)
-# secret_word = load_word()
-# spaceman(load_word())
+# letters_guessed = ["a"]
+# secret_word = "apple"
+# get_guessed_word(secret_word, letters_guessed)
+running = True
+while running:
+    secret_word = load_word()
+    spaceman(secret_word)
+    new_game = input("Play Again? (Y/N) ")
+    if "N" == new_game:
+        running = False
